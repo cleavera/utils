61443b89d82196af698666d2be063d565cef405a
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-11-22 15:49:31 +0000

    Improve prettify string

--- src/helpers/prettify-string.helper.ts
+++ src/helpers/prettify-string.helper.ts
@@ -1,6 +1,7 @@
 export function $prettifyString(str: string): string {
     return str
-        .replace('-', ' ')
+        .replace(/-/g, ' ')
+        .replace(/\./g, ' ')
         .replace(/^[a-z]/, (match: string) => {
             return match.toUpperCase();
         });

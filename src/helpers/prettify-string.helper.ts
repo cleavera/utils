a5c6fe1569cb7cf63b2009dbb9883bf10a78d6ad
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-11-30 15:24:52 +0000

    Prettify string to work on camel case

--- src/helpers/prettify-string.helper.ts
+++ src/helpers/prettify-string.helper.ts
@@ -2,6 +2,9 @@ export function $prettifyString(str: string): string {
     return str
         .replace(/-/g, ' ')
         .replace(/\./g, ' ')
+        .replace(/([a-z])([A-Z])/g, (_match: string, endOfLastWord: string, startOfNextWord: string) => {
+            return `${endOfLastWord} ${startOfNextWord.toLowerCase()}`;
+        })
         .replace(/^[a-z]/, (match: string) => {
             return match.toUpperCase();
         });

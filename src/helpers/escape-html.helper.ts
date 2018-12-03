7c42f99c69b4ada9add8503a247a33f9609f7be7
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-12-03 12:59:47 +0000

    Add escape html function

--- src/helpers/escape-html.helper.ts
+++ src/helpers/escape-html.helper.ts
@@ -1,5 +1,5 @@
 export function $escapeHTML(html: string): string {
     return html.replace(/[&<>"']/g, (char: string): string => {
-        return '&#' + char.charCodeAt(0) + ';';
+        return '&#' + char.charCodeAt(0).toString(10) + ';';
     });
 }

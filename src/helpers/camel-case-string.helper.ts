342ed36aa457da32cbc42649f0a7c37e8538fbd1
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-12-05 14:34:40 +0000

    Add camel case function

---
+++ src/helpers/camel-case-string.helper.ts
@@ -0,0 +1,9 @@
+export function $camelCaseString(input: string): string {
+    return input
+        .replace(/^[A-Z]/, (char: string) => {
+            return char.toLowerCase();
+        })
+        .replace(/([A-z]) ([A-z])/, (_match: string, m1: string, m2: string) => {
+            return `${m1}${m2.toUpperCase()}`;
+        });
+}

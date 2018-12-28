6843dab4e045ecdfc68fa5b04bfa2bb44a5f23aa
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-12-28 20:05:33 +0000

    Fix serializable default type

--- src/interfaces/serializable.interface.ts
+++ src/interfaces/serializable.interface.ts
@@ -1,6 +1,6 @@
 import { IDict } from './dict.interface';
 import { IJsonValue } from './json-value.interface';
 
-export interface ISerializable<T = IDict<IJsonValue>> {
+export interface ISerializable<T = IJsonValue | IDict<IJsonValue>> {
     serialize(): T;
 }
